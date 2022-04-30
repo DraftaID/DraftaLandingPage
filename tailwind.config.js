f629571df45257b8423a41aaea4cc2001918aafb
M. Fiqri Ananda <muh.fikri.ananda@gmail.com>
2022-04-30 23:48:11 +0700

    Desktop Landing Page 100%

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      spacing: {
+        '3275px': '3275px',  
+        '1420px': '1420px',
+        '3100px': '3100px',    
+      },
+    },
+    fontFamily: {
+      roboto: ['Roboto', 'sans-serif'],
+      productSansReg: ['Product Sans Regular'],
+      productSansBold: ['Product Sans Bold'],
+      poppins: ['Poppins', 'sans-serif'],
+      hind: ['Hind', 'sans-serif'],
+    },
+    borderRadius: {
+      '5-xl': '45px'
+    },
+  },
+  plugins: [],
+}
