ef356adb6ac82adba1300476f96eae95237f4b9b
M. Fiqri Ananda <muh.fikri.ananda@gmail.com>
2022-06-24 03:51:23 +0700

    Desktop Landing 98% done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,20 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      color: {
+        '#616161': '#616161'
+      },
       spacing: {
         '3275px': '3275px',  
         '1420px': '1420px',
         '3100px': '3100px',    
       },
+      lineHeight: {
+        '68': '68px'
+      },
+      dropShadow: {
+        '5xl': '0px 4px 4px rgba(0, 0, 0, 0.25)'
+      }
     },
     fontFamily: {
       roboto: ['Roboto', 'sans-serif'],
@@ -16,7 +25,8 @@ module.exports = {
       hind: ['Hind', 'sans-serif'],
     },
     borderRadius: {
-      '5-xl': '45px'
+      '5-xl': '45px',
+      '1.5-xl': '17.09px'
     },
   },
   plugins: [],
